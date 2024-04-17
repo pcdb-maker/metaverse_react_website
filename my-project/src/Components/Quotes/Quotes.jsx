3cf5790a9cf72e720e4dd97cbc4e3b876c2b9da1
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-17 14:31:18 +1000

    Quotes completed

--- my-project/src/Components/Quotes/Quotes.jsx
+++ my-project/src/Components/Quotes/Quotes.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const PopupPlayer = () => {
+const Quotes = () => {
   return (
-    <div>PopupPlayer</div>
-  )
-}
+    <>
+      <div className="container py-14 px-4">
+        <h1
+          data-aos="zoom-in"
+          className="text-2xl sm:text-4xl font-bold max-w-[650px] mx-auto text-center font-parisienne"
+        >
+          "Virtual reality is a medium, a means by which humans can share ideas
+          and experiences."
+        </h1>
+      </div>
+    </>
+  );
+};
 
-export default PopupPlayer
+export default Quotes;
